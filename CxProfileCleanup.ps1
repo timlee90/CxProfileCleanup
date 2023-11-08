377e208d65be2435097e04fa613082cbcc0155d5
TimLee90 <tim.joynson@yahoo.com>
2023-11-08 22:07:33 +0000

    Modified variables and added static variable for deleted months

--- CxProfileCleanup.ps1
+++ CxProfileCleanup.ps1
@@ -3,14 +3,18 @@ $ScriptPath = "C:\Scripts\ProfileCleanup\"
 $w7ProfilePath = "C:\Citrix\Profiles\Win7\"
 $w2012ProfilePath = "C:\Citrix\Profiles\w2012\"
 
-$LogDay = (Get-Date).Day
-$LogMonth = (Get-date).Month
-$LogYear = (Get-date).Year
-$LogTimeHours = (Get-Date).TimeOfDay.Hours
-$LogTimeMinutes = (Get-Date).Timeofday.Minutes
+$DeletionInMonths = "2" # Enter how far you want to go, e.g. 2 to go back 2 months.
 
+# Todays date with format YEAR/MONTH/DAY
 $LogTitle = get-date -format yyyymmdd-hhmm
 
-$DateCheck = (Get-Date).AddMonths(-1)
-write-verbose $DateCheck
-get-childitem -path $w7ProfilePath | ? {$_.CreationTime -lt (get-date).AddMonths(-1)} | remove-item -recurse -force -ErrorAction SilentlyContinue -verbose | set-content "$logtitle-ProfileCleanup.txt"
+# Date Check for User to ensure that script has written 
+$DateCheck = (Get-Date)
+$DateLessThan = (Get-Date).AddMonths(-$DeletionInMonths)
+
+write-host "Check current date and date with your chosen value."
+write-host "Todays date is $DateCheck."
+write-host "You're looking to delete folders older than this date: $DateLessThan, if this is incorrect, you have 10 seconds to CTRL+C"
+Start-Sleep 10
+
+get-childitem -path $w7ProfilePath | Where-Object {$_.CreationTime -lt (get-date).AddMonths(-$DeletionInMonths)} | remove-item -recurse -force -ErrorAction SilentlyContinue -verbose | set-content "$logtitle-ProfileCleanup.txt"
